6b9109871c553d9089eac3cf9150201aa447dadd
Elif Furuncu <eliffuruncu93@gmail.com>
2019-11-25 19:21:55 +0300

    first commit

---
+++ app/src/main/java/com/eliffuruncu/artbook/MainActivity.java
@@ -0,0 +1,95 @@
+package com.eliffuruncu.artbook;
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+    ArrayList<String> nameArray;
+    ArrayList<Integer> idArray;
+    ArrayAdapter arrayAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView=(ListView)findViewById(R.id.listView);
+        nameArray=new ArrayList<String>();
+        idArray=new ArrayList<Integer>();
+
+       arrayAdapter=new ArrayAdapter(this,android.R.layout.simple_list_item_1,nameArray);
+        listView.setAdapter(arrayAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent intent =new Intent(MainActivity.this,Main2Activity.class);
+                intent.putExtra("artId",idArray.get(i));
+                intent.putExtra("info","old");
+                startActivity(intent);
+            }
+        });
+        getData();
+
+    }
+    public void getData()
+    {
+       try{
+           SQLiteDatabase database=this.openOrCreateDatabase("Arts",MODE_PRIVATE,null);
+
+           Cursor cursor=database.rawQuery("SELECT * FROM arts",null);
+
+           int nameIx=cursor.getColumnIndex("artname");
+           int idIx=cursor.getColumnIndex("id");
+
+           while (cursor.moveToNext())
+           {
+               nameArray.add(cursor.getString(nameIx));
+               idArray.add(cursor.getInt(idIx));
+           }
+
+           arrayAdapter.notifyDataSetChanged();
+           cursor.close();
+       }catch (Exception e)
+       {
+           e.printStackTrace();
+       }
+    }
+
+
+    //hangi menuyu göstereceğiz onu belirliyoruz.
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        //Inflater
+        MenuInflater menuInflater=getMenuInflater();
+        menuInflater.inflate(R.menu.add_art,menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    //kullanıcı herhangi bir item ı seçerse ne yapıcaz onu belirliyoruz.
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.add_art_item)
+        {
+            Intent intent=new Intent(MainActivity.this,Main2Activity.class);
+            intent.putExtra("info","new");
+            startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
